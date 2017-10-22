71f6e0dac81114f055d55e5cc8b171544bbf6718
Plinz <antoine.duquennoy@protonmail.com>
2017-10-22 17:53:13 +0200

    Update test_trigger_cage_noGarde.sql

--- test_trigger_cage_noGarde.sql
+++ test_trigger_cage_noGarde.sql
@@ -1,15 +1,15 @@
 /*Test du troisième trigger avec un insert (trigger_cage_gardee)*/
 DELETE FROM LesAnimaux WHERE nomA = 'pattenrond';
-DELETE FROM LesCages WHERE noCage = 88;
 DELETE FROM LesGardiens WHERE noCage = 88;
+DELETE FROM LesCages WHERE noCage = 88;
 INSERT INTO LesCages VALUES (88, 'chat', 17);
 INSERT INTO LesAnimaux VALUES ('pattenrond', 'male', 'chat', 'flereur', 'angleterre', 1993, 88, 0);
 COMMIT;
 
 /*Test du troisième trigger avec un update (trigger_cage_gardee)*/
 DELETE FROM LesAnimaux WHERE nomA = 'pattenrond';
-DELETE FROM LesCages WHERE noCage = 88;
 DELETE FROM LesGardiens WHERE noCage = 89;
+DELETE FROM LesCages WHERE noCage = 88;
 DELETE FROM LesEmployes WHERE nomE = 'hermione';
 INSERT INTO LesEmployes VALUES ('hermione', 'angleterre');
 INSERT INTO LesGardiens VALUES (88, 'hermione');
